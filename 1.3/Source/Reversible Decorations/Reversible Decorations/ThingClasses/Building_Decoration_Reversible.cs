78aa917445d16e30b2c8387e5268ceeb32eb0aaf
Steve <stevenzybertusn@gmail.com>
2022-10-09 03:14:02 -0400

    everything is in the right method now
    -_-

--- 1.3/Source/Reversible Decorations/Reversible Decorations/ThingClasses/Building_Decoration_Reversible.cs
+++ 1.3/Source/Reversible Decorations/Reversible Decorations/ThingClasses/Building_Decoration_Reversible.cs
@@ -16,10 +16,13 @@ namespace Reversible_Decorations
         private Graphic ReversedGraphicCache;
         public Graphic reversedGraphic => ReversedGraphicCache;
         public Building_Decoration_Reversible_ModExtension BuildingExt;
-        public bool BackAndForth = true;
-        public int InterpolationPeriod = 2500 / Rand.Int;
-        public int Period = 0;
         private readonly float MaxAngle = 360.0f;
+        public int InterpolationPeriod = 30;
+        public int Period = 0;
+        private float NewXCoord = 0; // Initial value we don't care cause it get's updated in Tick()
+        private float ExtraRotation = 0; // Initial value we don't care cause it get's updated in Tick()
+
+
 
         public override void SpawnSetup(Map map, bool respawningAfterLoad)
         {
@@ -29,28 +32,48 @@ namespace Reversible_Decorations
         public override void Tick()
         {
             base.Tick();
+            // Do all the actual calculations here dumbass...
+            // and don't forget to store the results!
+
             float startMarker = DefaultGraphic.data.drawOffset.x; // starting x value
+            int curTick = GenLocalDate.DayTick(Map); // actual current tick of the current day
+            int curHour = GenLocalDate.HourOfDay(Map); // an int, 1 - 24
+            bool onTheHour = curTick % GenDate.TicksPerHour == 0; // is the curTick the start of a new hour in-game?
+
+            Color color1 = new(0.145f, 0.588f, 0.745f, 1f); // for debugging text
 
             if ((HitPoints < MaxHitPoints) && (BuildingExt.reversedGraphicData != null))
             {
-                if (BackAndForth == true) // move between two points
+                Period += Find.TickManager.TicksAbs;
+                if (Period <= InterpolationPeriod)
                 {
-                    Period += Find.TickManager.TicksAbs;
-                    if (Period <= InterpolationPeriod)
-                    {
-                        Period -= InterpolationPeriod;
+                    Period -= InterpolationPeriod;
+                    // oscillatingValue = [amplitude * Sin(rad * current tick / speed)] - [middle of sin wave]
+                    NewXCoord = (0.50f * Mathf.Sin(Mathf.PI * curTick / 120f) - startMarker);
+                    // rotationVaule = [(current tick * spin speed) per (360 degrees)]
+                    ExtraRotation = (Find.TickManager.TicksAbs * 0.5f) % (MaxAngle);
 
-                        // oscillatingValue = [amplitude * Sin(rad * current tick / speed)] - [middle of sin wave]
-                        reversedGraphic.data.drawOffset = new Vector3
-                            (0.50f * Mathf.Sin(Mathf.PI * Find.TickManager.TicksAbs / 120f) - startMarker, // x
-                            reversedGraphic.data.drawOffset.y, // y
-                            reversedGraphic.data.drawOffset.z); // z
-
-                        float extraRotation;
-                        extraRotation = (Find.TickManager.TicksAbs * 0.5f) % (MaxAngle);
-                        reversedGraphic.DrawWorker(reversedGraphic.data.drawOffset, Rotation, def, this, extraRotation);
-                    }
+                    // messages for debugging
+                    //Log.Message("x = " + NewXCoord.ToString().Colorize(color1) + " , Time of day = " + curHour.ToString().Colorize(color1));
                 }
+            }
+        }
+
+        public override void Draw()
+        {
+            base.Draw();
+            // do the actual fucking drawing here Steve...
+
+            if (BuildingExt.reversedGraphicData != null)
+            {
+                // for x movement
+                reversedGraphic.data.drawOffset = new Vector3 (
+                    NewXCoord, // x
+                    reversedGraphic.data.drawOffset.y, // y
+                    reversedGraphic.data.drawOffset.z); // z
+
+                // needed for rotation?
+                reversedGraphic.DrawWorker(DrawPos, Rotation, def, this, ExtraRotation);
             }
         }
 
